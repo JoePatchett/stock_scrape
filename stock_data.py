6f42d519129f2f14d0238ea0cfc1b172927ab6c1
JoePatchett <jtpatchett@gmail.com>
2018-01-19 23:20:10 -0500

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -25,4 +25,36 @@ def stock_get(nyse_codes, csv_file)
 			databox.append(parsed_text.findAll('div',{'class':'value__b93f12ea'})[i].string)
 		stock_data_writer.writerow()
 	stock_data_file.close()
+	
+def calculate_daily_differences(csv_file_day1, csv_file_day2):
+	"""
+	This function takes the stock data from one day, subtracts each value and appends the day 1, day 2, and delta value
+	into a list. The structure is organized as follows: [[Company 1, [day 1 value, day 2 value, delta], [], []...], [Company 2,...]]
+	"""
+	day1_values = []
+	day2_values = []
+	final_delta_values = []
+	with open(csv_file_day1, newline = '') as day1:
+	day_1_reader = csv.reader(day1, delimiter = ',')
+	for row in day_1_reader:
+		day1_values.append(row[:4])
+	with open(csv_file_day2, newline = '') as day2:
+	day_2_reader = csv.reader(day2, delimiter = ',')
+	for row in day_2_reader:
+		day2_values.append(row[:4])
+	delta_values = []
+	for i range(len(day_1_values)):
+		temp_list = []
+		for j in day_1_values[i]:
+			temp_delta = day_2_values[i][j]-day_1_values[i][j]
+			temp_list.append([day_1_values[i][j], day_2_values[i][j], temp_delta)
+		final_delta_values.append(temp_list)
+def kNN_comparison(test_point, rest_of_data):
+	"""
+	This machine learning algorithm takes in whatever stock data cal
+	"""
+					  
+					  
+			
+	
 
